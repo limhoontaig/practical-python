1d048bc299ff5ad289af4e7b13fca74fa527309c
limhoontaig <limhoontaig@gmail.com>
2021-06-06 10:12:17 +0900

    import related modified by lht

---
+++ Work/print-report.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# print-report.py
+
+import sys
+import os
+from porty.report import main
+
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+main(sys.argv)
