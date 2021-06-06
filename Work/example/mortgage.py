235882a9eb342d295783aa17c5a5c5ca5d2880a9
limhoontaig <limhoontaig@gmail.com>
2021-06-06 11:49:41 +0900

    add print format of monthly paid and remaining principal

--- Work/example/mortgage.py
+++ Work/example/mortgage.py
@@ -7,19 +7,20 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 extrapayment = 1000
-duration = 12
+extra_payment_start_month = 60
+extra_payment_end_month = 108
 total_paid = 0.0
-month = 1
+month = 0
 
 while principal > 0:
-    if month <= duration:
+    month += 1
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
         principal = principal * (1+rate/12) - payment - extrapayment
-        total_paid = total_paid + payment
+        total_paid = total_paid + payment +extrapayment
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
+    print(month, round(total_paid,2), round(principal,2))
 
-    month += 1
-
-print('Total paid', total_paid)
+print('Total paid', round(total_paid,2))
 print('Total month', month)
