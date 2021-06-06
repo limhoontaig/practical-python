9150caadaddf2809f0e6560f58d4574d1b521fa6
limhoontaig <limhoontaig@gmail.com>
2021-06-06 10:11:19 +0900

    modified import module

---
+++ Solutions/9_5/porty-app/porty/__init__.py
@@ -0,0 +1,13 @@
+# porty/__init__.py
+
+from .fileparse import parse_csv
+from . import follow
+from . import pcost
+from . import portfolio
+from . import report
+from . import stock
+from . import tableformat
+from . import test_stock
+from . import ticker
+# from . import typeproperty
+
--- Solutions/9_5/porty-app/print-report.py
+++ Solutions/9_5/porty-app/print-report.py
@@ -2,5 +2,11 @@
 # print-report.py
 
 import sys
+import os
+# import porty
 from porty.report import main
+
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
 main(sys.argv)
