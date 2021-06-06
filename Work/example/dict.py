8564189aea49c487b592529ccf7a9d19f28a9780
limhoontaig <limhoontaig@gmail.com>
2021-06-06 19:09:06 +0900

    Listcomprehension method

--- Work/example/dict.py
+++ Work/example/dict.py
@@ -5,11 +5,6 @@ f = open('Data/portfolio.csv')
 rows = csv.reader(f)
 f.close
 headers = next(rows)
-print(headers)
 total_cost=0
-for row in rows: # string data
-    cost = int(row[1]) * float(row[2])
-    total_cost += cost
-print('Using List :', total_cost)
-
-
+total_cost = sum([int(row[1]) * float(row[2]) for row in rows])
+print('Using List Comprehension:', total_cost)
