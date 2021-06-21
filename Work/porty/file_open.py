f2db2c84af2449e46f1c390d4b3afdbc4b79339e
limhoontaig <limhoontaig@gmail.com>
2021-06-21 20:53:39 +0900

    add def and sys

--- Work/porty/file_open.py
+++ Work/porty/file_open.py
@@ -1,8 +1,23 @@
-f = open('work/Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
-print(headers)
-for line in f:
-    row = line.split(',')
-    print(row)
-f.close()
+import csv
+import sys
 
+def portfolio_cost(filename):
+    total_cost=0
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    headers = next(rows)
+    print(headers)
+    for row in rows:
+        print(row)
+        total_cost += int(row[1]) * float(row[2])
+    f.close()
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'work/data/portfolio.csv'
+
+cost = portfolio_cost('work/data/portfolio.csv')
+print('Total cost : $',cost)
